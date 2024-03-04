8aa6cf1a6a335a868ddfb67845aeb9a2182bb29f
Adrian <adrian212005@yahoo.ro>
2024-03-04 16:49:06 +0200

    3 - Mail input and Explore the Solution Updates

--- components/ui/logo.tsx
+++ components/ui/logo.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Logo() {
   return (
     <Link href="/" className="block" aria-label="Cruip">
-      <img src="/images/companycompasslogo.jpg" height="50" width="50"></img>
+      <img src="/images/bbmmlogo.jpeg" height="50" width="50"></img>
     </Link>
   )
 }
