cbf494de8c59e6d49fc2dc9685b8512148275731
Mihnea <stamatiemihnea@gmail.com>
2024-03-04 21:53:03 +0200

    Hot Fix HTML CSS Errors

--- components/features.tsx
+++ components/features.tsx
@@ -73,7 +73,6 @@ export default function Features() {
                                             ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
                                             : "bg-gray-200 border-transparent"
                                     }`}
-                                    href="#0"
                                     onClick={(e) => {
                                         e.preventDefault();
                                         handleChangeTab(1);
@@ -92,13 +91,13 @@ export default function Features() {
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 24 24"
-                                            stroke-width="1.5"
+                                            strokeWidth="1.5"
                                             stroke="currentColor"
                                             className="w-6 h-6"
                                         >
                                             <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
                                                 d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
                                             />
                                         </svg>
@@ -111,7 +110,6 @@ export default function Features() {
                                             ? "bg-white shadow-md border-gray-200 hover:shadow-lg"
                                             : "bg-gray-200 border-transparent"
                                     }`}
-                                    href="#0"
                                     onClick={(e) => {
                                         e.preventDefault();
                                         handleChangeTab(2);
@@ -130,13 +128,13 @@ export default function Features() {
                                             xmlns="http://www.w3.org/2000/svg"
                                             fill="none"
                                             viewBox="0 0 24 24"
-                                            stroke-width="1.5"
+                                            strokeWidth="1.5"
                                             stroke="currentColor"
                                             className="w-6 h-6"
                                         >
                                             <path
-                                                stroke-linecap="round"
-                                                stroke-linejoin="round"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
                                                 d="m21 7.5-9-5.25L3 7.5m18 0-9 5.25m9-5.25v9l-9 5.25M3 7.5l9 5.25M3 7.5v9l9 5.25m0-9v9"
                                             />
                                         </svg>
