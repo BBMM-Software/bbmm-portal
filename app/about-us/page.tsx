cfc715952de4b3ac6cb89f444abc02378fef277b
Edi Bicu <bicuedici@gmail.com>
2024-03-13 21:50:59 +0200

    text about us

---
+++ app/about-us/page.tsx
@@ -0,0 +1,20 @@
+import Features from "@/components/features";
+import FeaturesBlocks from "@/components/features-blocks";
+import Hero from "@/components/hero";
+import Newsletter from "@/components/newsletter";
+import Testimonials from "@/components/testimonials";
+import React from "react";
+
+export default function AboutUsPage() {
+	return (
+		<section className="max-w-6xl mx-auto">
+			<div className="pt-32 md:pt-40">
+				<div>
+					Welcome to BBMM Software, where innovation meets expertise. We are a dynamic trio of freelancers who decided that together, we are stronger. Our journey began
+					as individual freelancers, each with our own set of skills, dreams, and aspirations. The realization that we could achieve so much more by combining our talents
+					led to the birth of BBMM Software.
+				</div>
+			</div>
+		</section>
+	);
+}
