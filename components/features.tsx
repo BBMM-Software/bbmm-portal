0a220dba255a5a052d239a3b31022c554a9f06f3
IoanaAdrian <adrian212005@yahoo.ro>
2024-03-15 10:21:51 +0200

    Hotfix: - revert colspan

--- components/features.tsx
+++ components/features.tsx
@@ -82,7 +82,7 @@ export default function Features() {
             <div className="absolute inset-0 bg-gray-100 pointer-events-none mb-16" aria-hidden="true"></div>
             <div className="absolute left-0 right-0 m-auto w-px p-px h-20 bg-gray-200 transform -translate-y-1/2"></div>
 
-            <div className="relative mx-auto pb-16">
+            <div className="relative max-w-6xl mx-auto px-4 pb-16 sm:px-6">
                 <div className="pt-12 md:pt-20">
                     {/* Section header */}
                     <div className="max-w-3xl mx-auto text-center md:pb-16">
