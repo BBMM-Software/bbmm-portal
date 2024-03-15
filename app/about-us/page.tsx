5ce2a1fbc7b4a46412c90f663e15008d58307950
Edi Bicu <bicuedici@gmail.com>
2024-03-15 11:07:42 +0200

    about us page completed

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -3,21 +3,13 @@ export const metadata = {
 	description: "About us",
 };
 
+import AboutUsCard from "../../components/ui/about-us-card/about-us-card";
+
 export default function AboutUsPage() {
 	return (
 		<section className="max-w-6xl mx-auto">
 			<div className="pt-32 md:pt-40">
-				<div className="w-full p-6 bg-white border border-gray-200 rounded-xl shadow text-center">
-					<h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-						About <span className="text-purple-200">us</span>
-					</h5>
-
-					<p className="mb-3 font-lg text-gray-700 dark:text-gray-400">
-						Welcome to BBMM Software, where innovation meets expertise. We are a dynamic trio of freelancers who decided that together, we are stronger. Our journey
-						began as individual freelancers, each with our own set of skills, dreams, and aspirations. The realization that we could achieve so much more by combining
-						our talents led to the birth of BBMM Software.
-					</p>
-				</div>
+				<AboutUsCard />
 			</div>
 		</section>
 	);
---
+++ components/ui/about-us-card/about-us-card.tsx
@@ -0,0 +1,81 @@
+"use client";
+
+import CountUp from "react-countup";
+import styles from "./about-us-card.module.css";
+import Image from "next/image";
+import AdrianIoana from "@/public/images/adrian-ioana.jpg";
+import EdiBicu from "@/public/images/edi-bicu.png";
+import MihneaStamatie from "@/public/images/mihnea-stamatie.png";
+import { useEffect, useState } from "react";
+
+const OUR_TEAM = [
+	{
+		img: AdrianIoana,
+		name: "Adrian Ioana",
+	},
+	{
+		img: EdiBicu,
+		name: "Eduard Bicu",
+	},
+	{
+		img: MihneaStamatie,
+		name: "Mihnea Stamatie",
+	},
+];
+
+export default function AboutUsCard() {
+	const [shuffledTeam, setShuffledTeam] = useState<typeof OUR_TEAM>([]);
+
+	useEffect(() => {
+		const team = [...OUR_TEAM];
+
+		for (let i = team.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
+			[team[i], team[j]] = [team[j], team[i]];
+		}
+
+		setShuffledTeam(team);
+	}, []);
+
+	return (
+		<div className="w-full p-6 py-12 bg-white sm:border sm:border-gray-200 rounded-xl shadow text-center sm:mx-2">
+			<h5 className="mb-2 text-4xl font-bold tracking-tight text-gray-900">
+				About <span className="bg-clip-text text-transparent bg-gradient-to-r from-purple-700 to-teal-400">us</span>
+			</h5>
+
+			<p className="my-5 text-lg text-gray-700">
+				Welcome to BBMM Software, where innovation meets expertise. We are a dynamic trio of freelancers who decided that together, we are stronger. Our journey began as
+				individual freelancers, each with our own set of skills, dreams, and aspirations. The realization that we could achieve so much more by combining our talents led to
+				the birth of BBMM Software.
+			</p>
+
+			<div className="flex justify-between text-center my-16 mx-auto sm:w-3/6">
+				{shuffledTeam.length > 0 ? shuffledTeam.map((member) => (
+					<div>
+						<div className={`${styles["image-container"]} ${styles.flipAnimation}`} >
+							<Image src={member.img} alt="co-founder-photo" className={styles["user-image"]} />
+						</div>
+						<h3>{member.name}</h3>
+					</div>
+				)) : <div className="w-full" style={{height: '136px'}}>
+					Team members</div>}
+			</div>
+
+			<h2 className="text-xl font-semibold">Satisfied Customers</h2>
+			<div className="my-5 mx-auto sm:w-3/6 flex justify-between">
+				<div>
+					<CountUp className="text-purple-700 text-2xl font-bold" start={0} end={10} duration={2} />
+					<h2 className="text-lg font-semibold">UpWork</h2>
+				</div>
+				<div>
+					<CountUp className="text-purple-700 text-2xl font-bold" start={0} end={80} duration={2} />
+					<h2 className="text-lg font-semibold">Independent Contracts</h2>
+				</div>
+				<div>
+					<CountUp className="text-purple-700 text-2xl font-bold" start={0} end={30} duration={2} />
+					<h2 className="text-lg font-semibold">Fiverr</h2>
+				</div>
+			</div>
+		</div>
+	);
+}
