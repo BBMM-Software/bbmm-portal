9cee2d4450170f01d307ce0b79b88a85513e33ac
Adrian <adrian212005@yahoo.ro>
2024-03-16 19:11:13 +0200

    Added JobPost and LinkedinAutomator projects

--- components/features.tsx
+++ components/features.tsx
@@ -9,6 +9,8 @@ import { Swiper, SwiperSlide } from "swiper/react";
 import Testimonial from "@/public/images/testimonial.jpg";
 import AbysGif from "@/public/projectGifs/abys.gif";
 import FriendzGif from "@/public/projectGifs/friendz.gif";
+import JobPostGif from "@/public/projectGifs/jobpost.gif"
+import LinkedinAutomatorPng from "@/public/projectGifs/linkedinautomator.png"
 import ContinuousTransitionIcon from "./ui/click-animation";
 import Utilities from "./utils/utilities";
 
@@ -48,6 +50,30 @@ const PROJECTS: Project[] = [
 			provided: ["Rough Figma design"],
 			delivered: ["Source code, 3D model, and assistance in hosting"],
 			techStack: ["Golang", "p5.js", "Autodesk Fusion"],
+		},
+	},
+	{
+		title: "Startup Finder",
+		description: "A web applications that connects software companies and potential employers. Users are able to search and filter through the jobs, and companies are able to post their job for a charge.",
+		gifs: [JobPostGif],
+		dev: {
+			time: "3 weeks",
+			request: ["modern UI", "search and filter for the jobs", "job post preview for the companies", "integration with stripe for payment"],
+			provided: ["hand-drawn sketches of the site's UI"],
+			delivered: ["Each change pushed on client's git server, dockerization and deployment on client's AWS account (frontend, backend, and database)"],
+			techStack: ["Angular 15", ".NET Core 6", "MySQL"],
+		},
+	},
+	{
+		title: "Linkedin Automator",
+		description: "A native MAC app that scrapes contacts for provided contacts from Linkedin. Users are able to reverse search the scraped contacts for certain keywords.",
+		gifs: [LinkedinAutomatorPng],
+		dev: {
+			time: "3 weeks",
+			request: ["MAC native application", "plug-and-play (doesn't require the user to download any 3rd party libraries)", "the scraping process should be shown to the user: each page should be opened in chrome", "reverse search functionality"],
+			provided: ["Figma design"],
+			delivered: ["Source code", "MAC executable", "assistance in setup"],
+			techStack: ["Swift UI", "Automator Scripts"],
 		},
 	},
 ];
